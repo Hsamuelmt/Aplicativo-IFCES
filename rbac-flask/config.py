a1479e122e5ac23b1682d987424251f360a4f2c5
Samuel Moreno Ramírez <71468965+Hsamuelmt@users.noreply.github.com>
2026-05-12 00:10:14 -0500

    solucion

--- rbac-flask/config.py
+++ rbac-flask/config.py
@@ -14,8 +14,8 @@ class Config:
     if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
     
-    DB_NAME = os.getenv("DB_NAME", "aplicativo_ifces")
-    DB_USER = os.getenv("DB_USER", "root")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "")
     DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
     DB_PORT = os.getenv("DB_PORT", "3306")
